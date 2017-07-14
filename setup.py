e616a58936d94873a5d2350c82a0ffc8be3799b3
tiwo <tiwocode@gmail.com>
2017-07-14 22:10:56 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+
+mingener = Extension('mingener',
+    sources = ['mingener.c'])
+
+setup(  name='mingener',
+        version='0.1',
+        description = 'an experimental CPython extension',
+        ext_modules = mingener,
+)
