fb0ab0491e7221593bbbb7b154e21ee57b8b01bc
tiwo <tiwocode@gmail.com>
2017-07-14 22:29:06 +0200

    first version with, ahem, "functionality"

---
+++ mingener.c
@@ -0,0 +1,35 @@
+#include <Python.h>
+#include <stdio.h>
+
+static PyObject *
+mingener_print_countdown(PyObject *sef, PyObject *args) {
+  unsigned char number;
+
+  if (!PyArg_ParseTuple(args, "b", &number)) {
+    return NULL;
+  }
+
+  for(; number; --number) {
+    fprintf(stdout, "%u to go...\n", number);
+  }
+
+  fprintf("done.\n");
+
+  /* return Py_BuildValue("i", 1); */
+  return Py_True;
+}
+
+
+static PyMethodDef mingenerMethods[]= {
+  {"print_countdown", mingener_print_countdown, METH_VARARGS, "count down on stdout"},
+  {NULL, NULL, 0, NULL}
+}
+
+MOD_INIT(mingener) {
+  PyObject *module;
+  MOD_DEF(module, "mingener", "close-to minimal extension module", mingnerMethods)
+
+  if (!m) return MOD_ERROR_VAL;
+
+  return MOD_SUCCESS_VAL(module);
+}
